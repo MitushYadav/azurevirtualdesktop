1b0e9442e2ca05957d2dba0c4397cf6aaa273050
Mitush <mitush.yadav@gmail.com>
2025-01-30 18:54:42 +0530

    fixed scripts

--- imagemgmt/azuredevops/scripts/ApplyWindowsUpdates.ps1
+++ imagemgmt/azuredevops/scripts/ApplyWindowsUpdates.ps1
@@ -13,30 +13,74 @@ function Write-Log
 Param ([string]$LogString)
 $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
 $LogMessage = "$Stamp $LogString"
+Write-Output $LogMessage
 Add-content $LogFile -value $LogMessage
 }
 
+[bool]$NugetFailed = $false
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# install the NuGet package provider if required
-if(-not(Get-PackageProvider -Name Nuget -ListAvailable -ErrorAction SilentlyContinue)) {
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Write-Log -LogString "Installed the Nuget Package Provider"
+try {
+    # install the NuGet package provider if required
+    if(-not(Get-PackageProvider -Name Nuget -ListAvailable -ErrorAction SilentlyContinue)) {
+        Write-Log "Nuget package provider not found. Installing."
+        $sourceArgs = @{
+            Name = 'nuget.org'
+            Location = 'https://api.nuget.org/v3/index.json'
+            ProviderName = 'NuGet'
+        }
+        Register-PackageSource @sourceArgs
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false -ErrorAction Stop
+        #Get-PackageProvider | Where-Object name -eq 'nuget' | Install-PackageProvider -Force -Confirm:$false -ErrorAction Stop #
+        Write-Log -LogString "Installed the Nuget Package Provider"
+    }
+}
+catch {
+    Write-log -LogString "ERROR: $($_.Exception.Message)"
+    $NugetFailed = $true
 }
 
-# Install the PSWindowsUpdate moduele
-if(-not(Get-Module -Name "PSWindowsUpdate" -ListAvailable -ErrorAction SilentlyContinue)) {
-    # Module is not installed
-    Install-Module -Name PSWindowsUpdate -Scope AllUsers -Force -Confirm:$false
-    Write-Log -LogString "Installed the PSWindowsUpdate module"
+# check if the package provider is installed.
+if(Get-PackageProvider -Name Nuget -ListAvailable -ErrorAction SilentlyContinue) {
+    Write-Log -LogString "Nuget package provider is verified installed."
+}
+else {
+    $NugetFailed = $true
+}
+
+try {
+    # Install the PSWindowsUpdate moduele
+    if(-not(Get-Module -Name "PSWindowsUpdate" -ListAvailable -ErrorAction SilentlyContinue)) {
+        Write-Log "PSWindowsUpdate module not found. Installing."
+        if($NugetFailed) {
+            Write-Log -LogString "Failed to install the Nuget provider. Exiting since PSWindowsUpdate module cannot be downloaded without this provider."
+            Exit
+        } else {
+            Install-Module -Name PSWindowsUpdate -Scope AllUsers -Force -Confirm:$false -ErrorAction Stop
+        }
+        
+        Write-Log -LogString "Installed the PSWindowsUpdate module."
+    }
+}
+catch {
+    Write-log -LogString "ERROR: $($_.Exception.Message)"
 }
 
 Import-Module PSWindowsUpdate
 Write-Log -LogString "Imported the PsWindowsUpdate module"
 
 try {
-    Install-WindowsUpdate -Install -MicrosoftUpdate -AcceptAll | Out-File "C:\Windows\Temp\$(get-date -f yyyy-MM-dd)-WindowsUpdate.log" -Force
-    Write-log -LogString "ran the windowsupdate part"
+    Add-WUServiceManager -MicrosoftUpdate -Silent -Confirm:$false
+    $updates = Get-WindowsUpdate
+    if($updates) {
+        Write-Log -LogString "Updates are available. Downloading and applying updates."
+        Install-WindowsUpdate -Install -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\Windows\Temp\$(get-date -f yyyy-MM-dd)-WindowsUpdate.log" -Force
+        Write-log -LogString "Installed WindowsUpdate."
+    }
+    else {
+        Write-Log -LogString "No updates to install."
+    }
 }
 catch {
     Write-Host "Error while updating Windows: $($_.Exception.Message)"
--- imagemgmt/azuredevops/scripts/CreateNewVMFromExistingVM.ps1
+++ imagemgmt/azuredevops/scripts/CreateNewVMFromExistingVM.ps1
@@ -5,25 +5,30 @@ This script creates a new VM from an existing VM.
 
 [CmdletBinding()]
 param (
-    [Parameter()]
+    [Parameter(Mandatory=$true)]
     [string]
     $SourceResourceGroupName,
-    [Parameter()]
+    [Parameter(Mandatory=$true)]
     [string]
     $SourceVMName,
-    [Parameter()]
+    [Parameter(Mandatory=$true)]
     [string]
     $DestinationResourceGroupName,
-    [Parameter()]
+    [Parameter(Mandatory=$true)]
     [string]
     $DestinationVMName,
-    [Parameter()]
+    [Parameter(Mandatory=$true)]
     [string]
     $DestinationVNetworkName,
-    [Parameter()]
+    [Parameter(Mandatory=$true)]
     [string]
-    $DestinationSubnetName
-
+    $DestinationSubnetName,
+    [Parameter(Mandatory=$false)]
+    [string]
+    $DestinationVmSize = "Standard_D2as_v5",
+    [Parameter(Mandatory=$false)]
+    [switch]
+    $CreatePublicIP
 )
 
 #region CreateSnapshot
@@ -31,50 +36,57 @@ $Location = (Get-AzResourceGroup -Name $SourceResourceGroupName).Location
 $snapshotName = "mySnapshot-$(get-date -f ddMMyyyyHHmm)"
 
 $srcVm = Get-AzVM -ResourceGroupName $SourceResourceGroupName -Name $SourceVMName
+$snapshot = New-AzSnapshotConfig -SourceUri $($srcVm.StorageProfile.OsDisk.ManagedDisk.Id) -Location $Location -CreateOption copy
 
-$snapshot = New-AzSnapshotConfig -SourceUri $srcVm.StorageProfile.OsDisk.ManagedDisk.Id -Location $Location -CreateOption copy
-
+# create the source VM disk snapshot in the same resource group as the VM.
 $vmSnapshot = New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $SourceResourceGroupName
 
 #endregion
 
 #region CreateVMFromSnapshot
-$vmSize = "Standard_D2as_v5"
-
-# new disk details
-$newOsDiskName = "myNewDisk01"
-$storageType = 'Premium_LRS'
-$diskSize = '128'
-
-# assuming a subnet and a vnet already exists
-$vNetName = "myVnet"
-$subnetName = "mySubnetName"
-
-#$snapshotObj = Get-AzSnapshot -SnapshotName $SnapshotName -ResourceGroupName $SourceResourceGroupName
-
-$diskConfig = New-AzDiskConfig -SkuName $storageType -Location $location -CreateOption Copy -SourceResourceId $vmSnapshot.Id -DiskSizeGB $diskSize
+$newOsDiskName = $($DestinationVMName + "-osdisk")
+$diskConfig = New-AzDiskConfig -SkuName $storageType -Location $location -CreateOption Copy -SourceResourceId $($vmSnapshot.Id) -DiskSizeGB $diskSize
 $newOsDisk = New-AzDisk -Disk $diskConfig -ResourceGroupName $DestinationResourceGroupName -DiskName $newOsDiskName
 
-# create a public IP for the VM
-$pipName = "myIP"
-$pip = New-AzPublicIpAddress -Name $pipName -ResourceGroupName $DestinationResourceGroupName -Location $location -AllocationMethod Static
+if($CreatePublicIP) {
+    # create a public IP for the VM
+    $pipName = $($DestinationVmName + "-pip")
+    $pip = New-AzPublicIpAddress -Name $pipName -ResourceGroupName $DestinationResourceGroupName -Location $location -AllocationMethod Static
+}
+
 
 # create a NIC and attach the public IP to it
-$nicName = "myNicName"
-$nsgName = "myNewNSG"
+$nicName = $($DestinationVMName + "-nic")
+$nsgName = $($DestinationVMName + "-nsg")
 
 $nsgObj = New-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $DestinationResourceGroupName -Location $Location
 $vNetObj = Get-AzVirtualNetwork -Name $DestinationVNetworkName
 $subnetObj = Get-AzVirtualNetworkSubnetConfig -Name $DestinationSubnetName -VirtualNetwork $vNetObj
-$nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $DestinationResourceGroupName -Location $location -SubnetId $subnetObj.Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsgObj.Id
+$nicOptions = @{
+    Name = $nicName
+    ResourceGroupName = $DestinationResourceGroupName
+    Location = $location
+    SubnetId = $subnetObj.Id
+    NetworkSecurityGroupId = $nsgObj.Id
+}
+
+if($CreatePublicIP) {
+    $nicOptions.Add("PublicIpAddressId", $pip.Id)
+}
+
+$nic = New-AzNetworkInterface @nicOptions
 
 # create the VM
 $vmConfig = New-AzVMConfig -VMName $DestinationVMName -VMSize $vmSize
 
 $vm = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
-$vm = Set-AzVMOSDisk -VM $vm -ManagedDiskId $newOsDisk.Id -StorageAccountType Standard_LRS -DiskSizeInGB 128 -CreateOption Attach -Windows
+$destVmStorageAccountType = $srcVm.storageprofile.osdisk.manageddisk.StorageAccountType
+$destVmOsDiskSize = $srcVm.StorageProfile.OsDisk.DiskSizeGB
+$vm = Set-AzVMOSDisk -VM $vm -ManagedDiskId $newOsDisk.Id -StorageAccountType $destVmStorageAccountType -DiskSizeInGB $destVmOsDiskSize -CreateOption Attach -Windows
 
 New-AzVM -ResourceGroupName $DestinationResourceGroupName -Location $location -VM $vm
+
+#endregion
 
 # rename new VM computer
 $sb = [scriptblock]{
